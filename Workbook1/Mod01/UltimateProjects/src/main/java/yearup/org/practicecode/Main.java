4aca249722dfdef99921bc128d7aebb2200bd7cd
Naomi Talukder <neoni@naomis-mbp.mynetworksettings.com>
2023-04-13 11:06:48 -0400

    initial commit

---
+++ Workbook1/Mod01/UltimateProjects/src/main/java/yearup/org/practicecode/Main.java
@@ -0,0 +1,23 @@
+package yearup.org.practicecode;
+
+class UltimateProjects {
+    public static void main(String[] args){
+
+        System.out.println("\u007b");// prints out value from ascii table ({ )
+        System.out.println("ring that bell");
+
+        System.out.println("\n\n\n");
+        System.out.println("Hello  "+ "earth"+" " +2.5);
+
+
+
+        System.out.print("Goodbye");// doesn't print on new line b/c no ln or \n
+        System.out.print("  ");
+        System.out.print("Everyone\n");
+
+
+        double price= 23.45668;
+        System.out.printf("Hello the cost is %8.2f ",  price);
+    }
+
+}
